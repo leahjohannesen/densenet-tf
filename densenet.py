c7639b9150e420b317e1cc5533f98d882f0c252d
LZK <loganzk@gmail.com>
2016-10-12 21:00:45 +0000

    Working with cifar10

--- densenet.py
+++ densenet.py
@@ -1,9 +1,24 @@
 import tensorflow as tf 
 from tensorflow.examples.tutorials.mnist import input_data
 
+'''
+This is my implementation of the densely connected fully convolutional network aka densenet.
+The paper describing the net can be found at:
+http://arxiv.org/abs/1608.06993
+The caffe model that I took from was implemented at:
+https://github.com/liuzhuang13/DenseNetCaffe/blob/master/make_densenet.py
+
+Main model hyperparameters:
+    depth - The total number of conv/pool layers in the model, must equal
+        2 (i/o) + 1 * [num_blocks-1] (transition) + num_blocks * block_layers (block layers)
+        In our default case, 7 = 2 + 1 * [2-1] + 2 * 2
+    first_output - number of layers in our initial convolution
+    growth_rate - the number of additional new filters to concatenate with our previous layer
+    
+'''
 #Basic functions for initializing layers
 def weights_init(shape):
-    return tf.Variable(tf.truncated_normal(shape))
+    return tf.Variable(tf.truncated_normal(shape, seed=1))
 
 def bias_init(shape):
     return tf.Variable(tf.constant(0.1))
@@ -15,70 +30,72 @@ def maxpool(x):
     return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding="VALID")
 
 def dropout(x, keep):
-    return tf.nn.dropout(x, keep)
+    return tf.nn.dropout(x, keep, seed=1)
 
 ##The slightly more complicated blocks of layers
 
 #Basic block of the model
 #Takes in a convolution layer, adds batch norm, relu, and a new convolution with dropout
-def bn_relu_conv(inputs, w_shape, b_shape, keep):
+def bn_relu_conv(inputs, w_shape, b_shape, keep, is_train):
     w = weights_init(w_shape) 
-    b = bias_init(b_shape) 
-    batch = tf.contrib.layers.batch_norm(inputs)
-    relu = tf.nn.relu(batch + b)
+    batch = tf.contrib.layers.batch_norm(inputs, is_training=is_train)
+    relu = tf.nn.relu(batch)
     conv = conv2d(relu, w)
     drop = dropout(conv, keep)
     return drop
 
 #Adds a batch norm/relu/conv layer and concatenates it with the input layer
-def add_layer(bottom, num_filter, keep):
+def add_layer(bottom, num_filter, keep, is_train):
     num_old = int(bottom.get_shape()[3])
-    brc = bn_relu_conv(bottom, [3,3,num_old,num_filter], [num_filter], keep)
+    brc = bn_relu_conv(bottom, [3,3,num_old,num_filter], [num_filter], keep, is_train)
     concat = tf.concat(3, [bottom, brc])
     return concat
 
 #The transition layer between blocks
 #Performs a bn/relu/conv and then a 2x2 maxpool to reduce the image size
-def transition(bottom, num_filter, keep):
+def transition(bottom, num_filter, keep, is_train):
     num_old = int(bottom.get_shape()[3])
-    brc = bn_relu_conv(bottom, [3,3,num_old,num_filter], [num_filter], keep)
+    brc = bn_relu_conv(bottom, [3,3,num_old,num_filter], [num_filter], keep, is_train)
     pool = maxpool(brc)
     return pool
 
-#The actual training step
-def train_model(depth=7, first_output=16, growth_rate=12, drop_num=0.5):
-    x = tf.placeholder(tf.float32, shape=[None, 784])
-    y = tf.placeholder(tf.float32, shape=[None, 10])
-    keep = tf.placeholder(tf.float32)
-    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-    
+#This portion puts the model together through the use of looping
+#I've sized it down from the original paper to fit on the smaller GPUs available
+def pred(x, keep, is_train, depth=40, first_output=16, growth_rate=12):
+
     n_classes = 10
-
     n_channels = first_output
-    #First conv layer
+    #First conv layer that starts the model
     with tf.variable_scope("input"):
-        x_in = tf.reshape(x, [-1,28,28,1])
-        w = weights_init([3,3,1,n_channels])
-        layer = conv2d(x_in, w)
+        w = weights_init([3,3,3,n_channels])
+        layer = conv2d(x, w)
 
-    N = (depth - 3)/2
+    N = (depth - 4)/3
 
     for i in range(1,N+1):
         name = "block1-{}".format(i)
         with tf.variable_scope(name): 
-            layer = add_layer(layer, n_channels, keep)
+            layer = add_layer(layer, n_channels, keep, is_train)
         n_channels += growth_rate
     with tf.variable_scope("trans1"):
-        layer = transition(layer, n_channels, keep)
+        layer = transition(layer, n_channels, keep, is_train)
 
-    for i in range(1,3):
+    for i in range(1,N+1):
         name = "block2-{}".format(i)
         with tf.variable_scope(name): 
-            layer = add_layer(layer, n_channels, keep)
+            layer = add_layer(layer, n_channels, keep, is_train)
+        n_channels += growth_rate
+    with tf.variable_scope("trans2"):
+        layer = transition(layer, n_channels, keep, is_train)
+
+    for i in range(1,N+1):
+        name = "block3-{}".format(i)
+        with tf.variable_scope(name): 
+            layer = add_layer(layer, n_channels, keep, is_train)
         n_channels += growth_rate
 
     with tf.variable_scope("output"):
-        batch = tf.contrib.layers.batch_norm(layer)
+        batch = tf.contrib.layers.batch_norm(layer, is_training=is_train)
         relu = tf.nn.relu(batch)
         rs = relu.get_shape()
         n_flat = int(rs[1])*int(rs[2])*int(rs[3])
@@ -88,23 +105,9 @@ def train_model(depth=7, first_output=16, growth_rate=12, drop_num=0.5):
         dense = tf.matmul(flat, w)
         output = tf.nn.softmax(dense + b)
 
-    cross_entropy = tf.reduce_mean(-tf.reduce_sum(y * tf.log(output), reduction_indices=[1]))
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+    return output
 
-    correct_prediction = tf.equal(tf.argmax(output,1), tf.argmax(y,1))
+def acc(y, y_pred):
+    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_pred,1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-    with tf.Session() as sess:
-        sess.run(tf.initialize_all_variables())
-        summary_writer = tf.train.SummaryWriter('../logs/', sess.graph)
-        for i in range(1000):
-            batch = mnist.train.next_batch(100)
-            if i%100 == 0:
-                train_accuracy = accuracy.eval(feed_dict={x:batch[0], y: batch[1], keep: 1})
-                print "Accuracy: {}".format(train_accuracy)
-            results = sess.run(train_step, feed_dict={x: batch[0], y: batch[1], keep: drop_num})
-
-        
-
-if __name__ == "__main__":
-    train_model()
+    return accuracy
---
+++ evalcifar10.py
@@ -0,0 +1,84 @@
+import tensorflow as tf
+import numpy as np
+import sys
+import densenet
+
+sys.path.append('/home/ubuntu/data-classes/')
+from cifar10 import cifar10
+
+def train(drop=0.5):
+    data = cifar10()
+
+    x = tf.placeholder(tf.float32, shape=[None, 32, 32, 3])
+    y = tf.placeholder(tf.float32, shape=[None, 10])
+
+    keep = tf.placeholder(tf.float32)
+    is_train = tf.placeholder(tf.bool)
+
+    y_pred = densenet.pred(x, keep, is_train)
+    acc = densenet.acc(y, y_pred)
+
+    loss = tf.nn.softmax_cross_entropy_with_logits(y_pred, y)
+    #This calls the optimizer from the opts.py module. Helps with clutter.
+    train_step = tf.train.AdamOptimizer().minimize(loss)
+
+    with tf.Session() as sess:
+        sess.run(tf.initialize_all_variables())
+
+        #Some constats/variables used in the training
+        epochs = 5
+        batch_size = 64 
+        total_train = len(data.x_trn)
+        deciles = int(total_train/(10*batch_size))
+
+        #Computes tstidation accuracy iteratively to avoid blowing up memory
+        print '-----Starting the Session-----'
+        tst_list = []
+        n_test = 0
+        while True:
+            batch_tst = data.next_tst(batch_size)
+            if not batch_tst:
+                break
+            tst_acc = sess.run(acc, feed_dict={x: batch_tst[0], y: batch_tst[1], 
+                                            keep: 1.0, is_train: False})
+            tst_list.append(tst_acc)
+            if n_test % 25 == 0: print 'Pic {}'.format(n_test * 64)
+            n_test += 1
+
+        print '\n' + '- '*10
+        print "Starting Test Accuray: {}".format(np.mean(tst_list))
+        print '- '*10 + '\n'
+        loss_list = []
+
+        #The actual training regimen
+        for epoch in range(epochs):
+            print "-----Starting Epoch {}-----".format(epoch)
+            n = 0
+
+            while True:
+                #Gets next batch of data, returns tuple of x/y if it hasn't gone through
+                #the epoch, otherwise returns false and goes into the tstidation regime
+                batch = data.next_trn(batch_size)
+                if not batch:
+                    tst_list = []
+                    while True:
+                        batch_tst = data.next_tst(batch_size)
+                        if not batch_tst:
+                            break
+                        tst_acc = sess.run(acc, feed_dict={x: batch_tst[0], y: batch_tst[1],
+                                                           keep: 1.0, is_train: False})
+                        tst_list.append(tst_acc)
+                    print "End of Epoch"
+                    print "Test Accuracy: {}\n".format(np.mean(tst_list))
+                    break
+
+                #Prints the status of the run, every 10%
+                if n%deciles == 0:
+                    print "Percent of epoch complete: {}0%.".format(n/deciles)
+                n += 1
+                loss_trn, _ = sess.run([loss, train_step], feed_dict={x: batch[0],
+                                                     y: batch[1], keep: drop, is_train: True})
+                loss_list.append(loss_trn)
+
+if __name__ == '__main__':
+    train()
